9d4e79e1c8fbcc5f8afbfdc2a5b3ecdccc93d697
Dileepa Madusanka <sanka.dmg@gmail.com>
2022-07-10 10:21:08 +0530

    main

--- backend/server.js
+++ backend/server.js
@@ -50,11 +50,7 @@ app.post("/addTeacher", async function (req, res) {
   console.log(
     "Request received to add teacher. Req body: " + JSON.stringify(reqBody)
   );
-  let data = await addTeacher(
-    reqBody.id, 
-    reqBody.name, 
-    reqBody.age
-    );
+  let data = await addTeacher(reqBody.id, reqBody.name, reqBody.age);
 
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
@@ -65,11 +61,7 @@ app.post("/editTeacher", async function (req, res) {
   console.log(
     "Request received to update teacher. Req body: " + JSON.stringify(reqBody)
   );
-  let data = await updateTeacher(
-    reqBody.name,
-    reqBody.age,
-    reqBody.id
-    );
+  let data = await updateTeacher(reqBody.name,reqBody.age,reqBody.id);
 
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
@@ -137,12 +129,7 @@ app.post("/editStudent", async function (req, res) {
   console.log(
     "Request received to update Student. Req body: " + JSON.stringify(reqBody)
   );
-  let data = await updateStudent(
-    reqBody.name,
-    reqBody.age,
-    reqBody.hometown,
-    reqBody.id
-    );
+  let data = await updateStudent(reqBody.name,reqBody.age,reqBody.hometown,reqBody.id);
 
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
